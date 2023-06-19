f8787b26495d7189d6e64e4d071721da8bd7de45
Vikram Kolupula <vikrampatelkolupula@gmail.com>
2023-06-19 02:02:49 +0100

    Interpolator

---
+++ Problem_72.m
@@ -0,0 +1,15 @@
+va = 2;  
+b = 2.^(1:10);
+a  = primes(30);
+%%
+
+isin = ismember(va,a);
+if isin == 1
+    n = find(a==va);
+    vb = b(n)
+else
+    right_bound = find(a > va, 1, 'first');
+    left_bound = find(a < va, 1, 'last') ; 
+    
+    vb = mean([b(left_bound) b(right_bound)])
+end
