69e13abc899fa15a06376764145e3f40d5306957
Vikram Kolupula <vikrampatelkolupula@gmail.com>
2023-06-29 19:05:03 +0100

    Interpolator
    You have a two vectors, a and b. They are monotonic and the same length. Given a value, va, where va is between a(1) and a(end), find the a(n) and a(n+1) that flank it. Now interpolate the value, vb, such that it is proportionally between b(n) and b(n+1).
    Note that va can land exactly on a value of a as well.

--- Problem_72.m
+++ Problem_72.m
@@ -6,10 +6,10 @@ a  = primes(30);
 isin = ismember(va,a);
 if isin == 1
     n = find(a==va);
-    vb = b(n)
+    vb = b(n);
 else
     right_bound = find(a > va, 1, 'first');
     left_bound = find(a < va, 1, 'last') ; 
     
-    vb = mean([b(left_bound) b(right_bound)])
+    vb = mean([b(left_bound) b(right_bound)]);
 end
